e8d10b38689bebc75e38a43e0bb11f62368ef04d
Gabriel Borges <161774213+devgabrielsborges@users.noreply.github.com>
2024-07-21 14:49:44 +0000

    Update funcs, main & Add requiments.txt

--- funcs.py
+++ funcs.py
@@ -2,13 +2,16 @@ import re
 import requests
 from bs4 import BeautifulSoup as bs
 
+# TODO: Criar funções para demais sites, respectivamente
+# TODO: Escrever documentação das funções
 
-def set_amazon(url: str):
+
+def set_amazon(url: str) -> dict:
     html_body = requests.get(url).content
     soup = bs(html_body, 'html.parser')
     price_search = soup.find('span', class_='a-offscreen')
     price = re.findall(re.compile(r'R\$(\d+\.\d+)'), price_search.text)[0]
-    name = soup.find('span', id='productTitle').get_text()
+    name = soup.find('span', id='productTitle').get_text().strip()
 
     return {
         'Link': url,
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import firebase_admin
 from firebase_admin import db, credentials
 from funcs import set_amazon
 
-load_dotenv()
+load_dotenv()   # Load environment variables
 
 cred = credentials.Certificate('firebase_credentials.json')
+#   Initialize Firebase app with credentials and database URL
 firebase_admin.initialize_app(
     cred,
     {
@@ -14,26 +15,36 @@ firebase_admin.initialize_app(
     }
 )
 
+#   Open file with links
 with open('links.txt', 'r') as file:
     links = file.readlines()
     product_info = {}
 
-    for link in links:
+    #   Loop for each link in file
+    for link_index, link in enumerate(links):
         index = db.reference('/last_item').get()
         try:
+            #   Check if link is from Amazon
             if 'amzn' in link:
                 product_info = set_amazon(link)
                 print(product_info)
 
         except Exception as e:
+            #   Write error in file
             with open('errors.txt', 'a') as error_file:
-                error_file.write(f'Error: {e}\nLink: {link}\n\n')
+                error_file.write(
+                    f'Error: {e}\nLink: {link}\n'
+                    f'Index: {link_index + 1}\n\n'
+                )
             continue
-        db.reference(f'/itens_amazon/{index + 1}').update(
+        #   Update database with product info
+        db.reference(f'/itens/{index + 1}').update(
             {
                 'Link': product_info['Link'],
                 'Produto': product_info['Produto'],
-                'Valor': product_info['Valor']
+                'Valor': product_info['Valor'],
+                'Ultimo_valor': product_info['Valor']
             }
         )
+        #   Update last item index
         db.reference('/last_item').set(index + 1)
