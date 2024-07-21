e9d3077e1b77db369642934b4404679a720c8781
Gabriel Borges <161774213+devgabrielsborges@users.noreply.github.com>
2024-07-21 14:50:29 +0000

    Update funcs & main

--- funcs.py
+++ funcs.py
@@ -6,15 +6,14 @@ from bs4 import BeautifulSoup as bs
 # TODO: Escrever documentação das funções
 
 
-def set_amazon(url: str) -> dict:
+def set_amazon(url: str):
     html_body = requests.get(url).content
     soup = bs(html_body, 'html.parser')
-    price_search = soup.find('span', class_='a-offscreen')
-    price = re.findall(re.compile(r'R\$(\d+\.\d+)'), price_search.text)[0]
-    name = soup.find('span', id='productTitle').get_text().strip()
 
+    price_search = soup.find('span', class_='a-offscreen')
+    price = re.findall(re.compile(r'R\$(\d+.\d+)'), price_search.text)[0]
+
+    name = soup.find('span', id='productTitle').get_text().strip()
     return {
-        'Link': url,
-        'Produto': name,
-        'Valor': float(price.replace(',', '.'))
+        'Link': url, 'Produto': name, 'Valor': float(price.replace(',', '.'))
     }
--- main.py
+++ main.py
@@ -15,29 +15,21 @@ firebase_admin.initialize_app(
     }
 )
 
-#   Open file with links
 with open('links.txt', 'r') as file:
     links = file.readlines()
     product_info = {}
 
-    #   Loop for each link in file
-    for link_index, link in enumerate(links):
+    for num, link in enumerate(links):
         index = db.reference('/last_item').get()
         try:
-            #   Check if link is from Amazon
             if 'amzn' in link:
                 product_info = set_amazon(link)
                 print(product_info)
 
         except Exception as e:
-            #   Write error in file
             with open('errors.txt', 'a') as error_file:
-                error_file.write(
-                    f'Error: {e}\nLink: {link}\n'
-                    f'Index: {link_index + 1}\n\n'
-                )
+                error_file.write(f'Error: {e}\nLink: {link}\n\n')
             continue
-        #   Update database with product info
         db.reference(f'/itens/{index + 1}').update(
             {
                 'Link': product_info['Link'],
@@ -46,5 +38,4 @@ with open('links.txt', 'r') as file:
                 'Ultimo_valor': product_info['Valor']
             }
         )
-        #   Update last item index
         db.reference('/last_item').set(index + 1)
